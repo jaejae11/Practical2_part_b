528a15dabc9563559a427595a8a2d04b54fd1b0b
jaejae11 <looijt-wm19@student.tarc.edu.my>
2020-07-02 01:31:52 +0800

    commit by Jia Toong

--- app/src/main/java/com/example/practical2_HelloConstraint/MainActivity.java
+++ app/src/main/java/com/example/practical2_HelloConstraint/MainActivity.java
@@ -1,15 +1,18 @@
-package com.example.practical2_part_a;
+package com.example.practical2_HelloConstraint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +31,26 @@ public class MainActivity extends AppCompatActivity {
         ++mCount;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+
+        btn=findViewById(R.id.button_zero);
+
+        if (mCount != 0)
+            btn.setBackgroundColor(Color.GREEN);
+
+        if(mCount % 2 == 0)
+            view.setBackgroundColor(Color.CYAN);
+        else if(mCount % 2 != 0)
+            view.setBackgroundResource(R.color.colorPrimary);
+
+    }
+
+    public void showZero(View view){
+        mCount=0;
+        mShowCount.setText(Integer.toString(mCount));
+        btn=findViewById(R.id.button_count);
+        if(mCount == 0) {
+            view.setBackgroundColor(Color.LTGRAY);
+            btn.setBackgroundResource(R.color.colorPrimary);
+        }
     }
 }
